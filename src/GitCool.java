b28e765dcc53edf88b1f5b39b603f1f9c8a774b6
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-19 16:28:49 -0400

    added one code into main method

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,6 +3,7 @@ public class GitCool {
 
         System.out.println("B23 is learning git");
         System.out.println("B23 is making some sense now");
+        System.out.println(" Iam gonna make this, whatever it takes");
 
     }
 }
