6a3bcee4177ba5a7a6ad8460508892845a9a2b96
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-19 16:43:01 -0400

    added one github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -5,5 +5,8 @@ public class GitCool {
         System.out.println("B23 is making some sense now");
         System.out.println(" Iam gonna make this, whatever it takes");
 
+        //We have added our GitHub account under IntelliJ
+        //make sure to click apply and click on to save
+
     }
 }
